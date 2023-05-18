479f8aa393708d5a6805d32a9ade84cddcffcf31
seijiro <modeverv@gmail.com>
2023-05-18 22:41:30 +0900

    2023-05-18 22:41:30

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 //    printf("Hello, World!\n");
@@ -34,10 +36,52 @@ int main() {
 //    return 0;
 //    return 0;
 
-    int i = 3;
-    do
+//    int i = 3;
+//    do
+//    {
+//        printf("%d ", --i);
+//    } while (i >= 0);
+//    int ia[] = { 3, 4, 5 };
+//    printf("%d\n", ia[2]);
+//    return 0;
+//    int ia[] = { 1, 2, 3, 4 };
+//    int i;
+//    for (i = 0; i <= 2; ++i)
+//    {
+//        ia[i + 1] += ia[i];
+//    }
+//    printf("%d\n", ia[3]);
+//    srand((unsigned int)time(NULL));
+//    int i;
+//    for (i = 0; i < 100; ++i)
+//    {
+//        printf("%d\n", 1 + rand() % 10);
+//    }
+//    return 0;
+
+
+
+//    int ia[] = { 3, 4, 5 };
+//    printf("%d\n", ia[2]);
+//    return 0;
+
+    double da[] = { 1.1, 2.5, 3.0, 4.7, 5.2 }, x;
+    int len, i;
+    len = (int)(sizeof da / sizeof da[0]); // 配列の長さ
+    for (x = 0, i = 0; i < len; ++i)
     {
-        printf("%d ", --i);
-    } while (i >= 0);
+        x += da[i];
+    }
+    // sumとって割るので平均
+    x /= len;
+    printf("%f\n", x);
+
+
+
+
+
+
+
+
 
 }
